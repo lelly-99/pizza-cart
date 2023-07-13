12e825c0510e76896149f91dd9e6b33cd5f50c45
progress <leswifil@gmail.com>
2023-07-13 12:43:16 +0200

    tofixed

--- pizza.js
+++ pizza.js
@@ -35,50 +35,50 @@ document.addEventListener('alpine:init', () => {
             }, 2000);
         },
         addSmall() {
-            this.small += 39.00;
+            this.small += 39.99;
             this.count += 1;
-            this.price += 39.00;
+            this.price += 39.99;
             this.size.push('small');
         },
         removeSmall() {
             if (this.count > 0 && this.size.includes('small')) {
-                this.price -= 39.00;
-                this.small -= 39.00;
+                this.price -= 39.99;
+                this.small -= 39.99;
                 this.count--;
                 this.size.splice(this.size.indexOf('small'), 1);
             }
         },
         addMedium() {
             this.count++;
-            this.price += 89.00;
-            this.medium += 89.00;
+            this.price += 89.99;
+            this.medium += 89.99;
             this.size.push('medium');
         },
         removeMedium() {
             if (this.count > 0 && this.size.includes('medium')) {
-                this.price -= 89.00;
-                this.medium -= 89.00;
+                this.price -= 89.99;
+                this.medium -= 89.99;
                 this.count--;
                 this.size.splice(this.size.indexOf('medium'), 1);
             }
         },
         addLarge() {
             this.count++;
-            this.price += 129.00;
-            this.large += 129.00;
+            this.price += 129.99;
+            this.large += 129.99;
             this.size.push('large');
         },
         removeLarge() {
             if (this.count > 0 && this.size.includes('large')) {
-                this.price -= 129.00;
-                this.large -= 129.00;
+                this.price -= 129.99;
+                this.large -= 129.99;
                 this.count--;
                 this.size.splice(this.size.indexOf('large'), 1);
             }
         },
         checkout() {
             if (this.paymentAmount >= this.price) {
-                this.checkoutMessage = "Payment successful and your change is R" + (this.paymentAmount - this.price)
+                this.checkoutMessage = "Payment successful and your change is R" + (this.paymentAmount - this.price).toFixed(2)
                 this.count = 0;
                 this.price = 0;
                 this.size = [];
@@ -92,7 +92,7 @@ document.addEventListener('alpine:init', () => {
                 this.paymentAmount = 0;
                 setTimeout(() => {
                     this.checkoutMessage = "";
-                }, 2000);
+                }, 4000);
             } else if (this.paymentAmount <= this.price) {
                 this.errorMessage = "Insufficient funds!";
                 setTimeout(() => {
